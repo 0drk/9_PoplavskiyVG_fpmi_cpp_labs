7c8845935730ad1a6d65da9351ac943852c9a45d
drk <sewap08@gmail.com>
2025-09-16 15:51:37 +0300

    asdasd

---
+++ semester_1/lab1_introduction/dz1/lab6/lab6/lab6/lab6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    setlocale(LC_ALL, "russian");
+    int n, f, k, S;
+    S = 0;
+    std::cout << "Введите число n для нахождения суммы первых n-нечётных чисел \n";
+    std::cin >> n;
+    if (n < ( - 1) || std::cin.fail())
+    {
+        std::cout << "Введено неправильное значение n";
+        exit(1);
+    }
+
+    k = 1;
+    f = n;
+    while (f > 0)
+    {
+        S += k;
+        k += 2;
+        f--;
+    }
+    std::cout << "Сумма первых " << n << " нечётных чисел равна " << S;
+
+
+}
+
