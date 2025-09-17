e075a3427695067ace46e08531b96acb8ff0c697
drk <sewap08@gmail.com>
2025-09-17 12:16:22 +0300

    zadanie4

---
+++ semester_1/lab2_integer_arithmetics/lab2_1/lab2_1/lab2_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n;
+	std::cout << "Введите число n: \n";
+	std::cin >> n;
+	if (n < 0 || std::cin.fail())
+	{
+		std::cout << "Введено неправильное значение n";
+		exit(1);
+	}
+	int a = 2;
+	int d = 1;
+	int S = 0;
+	std::cout << "Все совершенные числа, не превосходящие " << n << " : \n";
+	while (a <= n)
+	{
+	
+		while (d <  a)
+		{
+			if (a % d == 0)
+			{
+				S += d;
+			}
+			d++;
+		}
+		if (S == a)
+		{
+			std::cout << a << std::endl;
+		}
+		a++;
+		S = 0;
+		d = 1;
+	}
+}
+
