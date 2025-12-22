92974269b693c07d85753509abc930987367add1
0drk <0drk000@gmail.com>
2025-12-22 19:47:08 +0300

    vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,57 @@
 #pragma once
 
+#ifndef VECTOR_IMPL_H
+#define VECTOR_IMPL_H
+
+#include <ostream>
+#include <initializer_list>
+#include <stdexcept>
+#include <cstddef>
+
+
 class Vector {
-    // your code goes here
+private:
+    int* data_;
+    std::size_t size_;
+    std::size_t capacity_;
+
+    void reallocate(std::size_t new_capacity);
+
+public:
+   
+
+    //конструкторы
+    Vector();
+    Vector(std::size_t size);
+    Vector(std::initializer_list<int> init);
+    Vector(const Vector& other);
+
+
+    // Оператор присваивания
+    Vector& operator=(const Vector& other);
+
+    ~Vector();
+
+    
+
+    int& operator[](std::size_t index);
+    const int& operator[](std::size_t index) const;
+
+    int& At(std::size_t index);
+    const int& At(std::size_t index) const;
+
+    std::size_t Size() const;
+    std::size_t Capacity() const;
+
+    //методы
+    void Swap(Vector& other);
+    void PushBack(int value);
+    void PopBack();
+    void Clear();
+    void Reserve(std::size_t new_capacity);
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector& v);
 };
+
+#endif  ;
+
